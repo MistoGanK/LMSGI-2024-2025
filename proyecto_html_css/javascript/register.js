fe2e5adcbe9c29827fa88f8f5c1b99cc77cd222a
MistoGanK <nikipetkovv00@gmail.com>
2025-05-23 00:00:49 +0200

    update

--- proyecto_html_css/javascript/register.js
+++ proyecto_html_css/javascript/register.js
@@ -9,8 +9,13 @@ let html_form = document.getElementsByName('form');
 // CSS classes
 let jsPassWordValidator = document.querySelector('.jsPassWordValidator');
 
-// Elements for CSS
-let pMin;
+// HTML passErrorDiv
+let pMin = document.getElementById('pMin');
+let pMax = document.getElementById('pMax');
+let pUp = document.getElementById('pUp');
+let pNum = document.getElementById('pNum')
+let pLow = document.getElementById('pLow');
+let pSpe = document.getElementById('pSpe');
 
 // Submit
 let submit_button = document.getElementById('submit_button');
@@ -39,6 +44,7 @@ function passNumber(pass){
 function passSpecialLetter(pass){
   return /[^a-zA-Z0-9]/.test(pass);
 };
+let validate_style = document.querySelector('.jsValidated');
 
 // Password Booleans
 let passWordChecks = {
@@ -49,36 +55,58 @@ let passWordChecks = {
   boolNumber: false,
   boolSpecial: false,
 }
+// Show preset validation password
+form_input_password.addEventListener('focus',(e)=>{
+  form_input_password.classList.add("unvalidatedPassword");
+  jsPassWordValidator.classList.add("showContainer");
+});
+form_input_password.addEventListener('blur',(e)=>{
+  form_input_password.classList.remove("unvalidatedPassword");
+  jsPassWordValidator.classList.remove("showContainer");
+});
 
 form_input_password.addEventListener('keyup',(e)=>{
+  // Min
   if (!passLenghtMin(form_input_password.value)){
-    console.log("Longitud minima de 5");
-    form_input_password.classList.add("unvalidatedPassword");
-    jsPassWordValidator.classList.add("showContainer");
-  }else{
-    form_input_password.classList.remove("unvalidatedPassword");
-    jsPassWordValidator.classList.remove("showContainer");
+    pMin.classList.remove('jsValidated');
+  }else{  
     passWordChecks.boolLengthMin = true;
+    pMin.classList.add('jsValidated');
   }
+  // Max
   if(!passLenghtMax(form_input_password.value)){
+    pMax.pMax.classList.remove('jsValidated')
   }else{
     passWordChecks.boolLengthMax = true;
+    pMax.classList.add('jsValidated');
   }
+  // Lower case
   if(!passLowerCase(form_input_password.value)){
+    pLow.classList.remove('jsValidated');
   }else{
     passWordChecks.boolLowerCase = true;
+    pLow.classList.add('jsValidated');
   }
+  // Upper case
   if(!passUpperCase(form_input_password.value)){
+    pUp.classList.remove('jsValidated');
   }else{
     passWordChecks.boolUpperCase = true;
+    pUp.classList.add('jsValidated');
   }
+  // Number
   if(!passNumber(form_input_password.value)){
+    pNum.classList.remove('jsValidated');
   }else{
     passWordChecks.boolNumber = true;
+    pNum.classList.add('jsValidated');
   }
+  // Special
   if(!passSpecialLetter(form_input_password.value)){
+    pSpe.classList.remove('jsValidated');
   }else{
     passWordChecks.boolSpecial = true;
+    pSpe.classList.add('jsValidated');
   }
 });
 
@@ -90,13 +118,10 @@ register_form.addEventListener('submit', (e) => {
   console.log('Submit event triggered.');
 
   for (let i of Object.values(passWordChecks)) {
-    console.log('Checking condition:', i);
     if (!i) {
-      console.log('Password does not meet all requirements.');
       return; 
     }
   }
-
   // If all true submit the form
   console.log('All conditions met. Submitting the form...');
   register_form.submit(); 
