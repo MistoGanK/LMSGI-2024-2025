8c6b1d65c66559709a4cf048f75e4f06396849b0
MistoGanK <nikipetkovv00@gmail.com>
2025-05-22 19:48:58 +0200

    update

--- proyecto_html_css/javascript/register.js
+++ proyecto_html_css/javascript/register.js
@@ -1,9 +1,19 @@
 // HTML VARIABLES
+let register_form = document.getElementById('register_form');
+// Inputs
 let form_input_usuario = document.getElementById('usuario');
 let form_input_email = document.getElementById('email');
 let form_input_password = document.getElementById('password');
 let html_form = document.getElementsByName('form');
+
+// CSS classes
 let jsPassWordValidator = document.querySelector('.jsPassWordValidator');
+
+// Elements for CSS
+let pMin;
+
+// Submit
+let submit_button = document.getElementById('submit_button');
 
 // Password Section
 form_input_password.addEventListener('paste',(e)=>{
@@ -27,8 +37,18 @@ function passNumber(pass){
   return /[0-9]/.test(pass);
 };
 function passSpecialLetter(pass){
-  return /^[a-zA-Z0-9]/.test(pass);
+  return /[^a-zA-Z0-9]/.test(pass);
 };
+
+// Password Booleans
+let passWordChecks = {
+  boolLengthMax: false,
+  boolLengthMin: false,
+  boolLowerCase: false,
+  boolUpperCase: false,
+  boolNumber: false,
+  boolSpecial: false,
+}
 
 form_input_password.addEventListener('keyup',(e)=>{
   if (!passLenghtMin(form_input_password.value)){
@@ -38,7 +58,46 @@ form_input_password.addEventListener('keyup',(e)=>{
   }else{
     form_input_password.classList.remove("unvalidatedPassword");
     jsPassWordValidator.classList.remove("showContainer");
-
+    passWordChecks.boolLengthMin = true;
   }
-  console.log(form_input_password.value) // Debug
+  if(!passLenghtMax(form_input_password.value)){
+  }else{
+    passWordChecks.boolLengthMax = true;
+  }
+  if(!passLowerCase(form_input_password.value)){
+  }else{
+    passWordChecks.boolLowerCase = true;
+  }
+  if(!passUpperCase(form_input_password.value)){
+  }else{
+    passWordChecks.boolUpperCase = true;
+  }
+  if(!passNumber(form_input_password.value)){
+  }else{
+    passWordChecks.boolNumber = true;
+  }
+  if(!passSpecialLetter(form_input_password.value)){
+  }else{
+    passWordChecks.boolSpecial = true;
+  }
+});
+
+// Submit Control
+
+register_form.addEventListener('submit', (e) => {
+  // Checks if any boolean is false
+  e.preventDefault(); 
+  console.log('Submit event triggered.');
+
+  for (let i of Object.values(passWordChecks)) {
+    console.log('Checking condition:', i);
+    if (!i) {
+      console.log('Password does not meet all requirements.');
+      return; 
+    }
+  }
+
+  // If all true submit the form
+  console.log('All conditions met. Submitting the form...');
+  register_form.submit(); 
 });
